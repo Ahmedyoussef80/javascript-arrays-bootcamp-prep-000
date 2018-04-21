f9ff3cfba9a4657823671151254b8befabe65ea5
ahmed youssef elsayed <ahmedyoussef59@yahoo.com>
2018-04-21 00:26:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,7 +25,12 @@ function destructivelyRemoveElementFromEndOfArray(Array){
   return Array;
 }
   
+<<<<<<< HEAD
 function  removeElementFromEndOfArray(Array) {
 return  Array.slice(0, Array.length - 1);}
   
 
+=======
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);}
+>>>>>>> 74872051b02c7763c52ef3772c4f5ef30db52ea1
